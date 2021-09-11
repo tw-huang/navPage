2a77a1b4cf08257e0857f55fdfa685f59e895c7a
tw.huang <tw.huang@foxmail.com>
2021-09-11 12:55:45 +0800

    #二级路径#

--- navpage-ui/src/index.tsx
+++ navpage-ui/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // @ts-ignore
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
-	<BrowserRouter>
+	<HashRouter>
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>
-	</BrowserRouter>,
+	</HashRouter>,
 	document.getElementById('root')
 )
